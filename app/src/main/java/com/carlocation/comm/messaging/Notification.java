c62e4879a92d9e37d7df51e1f401705f7474e6be
JackyShen <hgshen2013@sina.com>
2015-04-21 17:26:18 +0800

    add constructor
    add constructor

--- app/src/main/java/com/carlocation/comm/messaging/Notification.java
+++ app/src/main/java/com/carlocation/comm/messaging/Notification.java
@@ -1,10 +1,12 @@
 package com.carlocation.comm.messaging;
 
 public class Notification {
+    public Notification(){
+        super();
+    }
 	
 	public Message message;
-	
-	
+
 	public enum NotificationType {
 		REQUEST,
 		RESPONSE,

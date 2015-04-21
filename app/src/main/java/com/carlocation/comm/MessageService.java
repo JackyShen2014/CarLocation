fd9fb50fd0ba38a46563382a350ba8ad14609fc4
Jiang Hunk <hunk.jiang@sonymobile.com>
2015-04-21 17:21:35 +0800

    Add rabbitmq library

--- app/src/main/java/com/carlocation/comm/MessageService.java
+++ app/src/main/java/com/carlocation/comm/MessageService.java
@@ -1,5 +1,6 @@
 package com.carlocation.comm;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,35 +17,70 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.carlocation.comm.messaging.AuthMessage;
 import com.carlocation.comm.messaging.Message;
+import com.carlocation.comm.messaging.MessageType;
+import com.carlocation.comm.messaging.Notification;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 /**
  * Communication service.<br>
- * <ul>Start service:</ul>
- * <ul>When Service destroyed, will send broadcast: com.carlocation/com.carlocation.MessageServiceDown</ul>
- * <ul>Must use permission for this service {@code <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/> }.<br>
- *     If connection state changed, will send broadcast: category: com.carlocation/com.carlocation.connection_state_changed with state code.
+ * <ul>
+ * Start service:
+ * </ul>
+ * <ul>
+ * When Service destroyed, will send broadcast:
+ * com.carlocation/com.carlocation.MessageServiceDown
+ * </ul>
+ * <ul>
+ * Must use permission for this service
+ * {@code <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/> }
+ * .<br>
+ * If connection state changed, will send broadcast: category:
+ * com.carlocation/com.carlocation.connection_state_changed with state code.
  * </ul>
  * 
  * @see ConnectionState
  * @author 28851274
- *
+ * 
  */
 public class MessageService extends Service {
-	
+
+	private static final String TAG = "CarMessageService";
+
 	/**
 	 * 
 	 */
 	public static final String EXTRA_CONNECTION_STATE = "state";
 
-	private static final String TAG = "CarMessageService";
-	
+	public static final String EXTRA_CONNECTION_SERVER_ADDR = "server_addr";
+
+	public static final String EXTRA_CONNECTION_SERVER_PORT = "server_port";
+
+	private String mServer;
+
+	private int mPort;
+
+	private String mUserName;
+
+	private String mPassword;
+
+	private ConnectionFactory connectionFactory;
+
+	private Connection mConnection;
+
+	private Channel mSendChannel;
+
 	private List<NotificationListener> notificationListeners = new ArrayList<NotificationListener>();
-	
+
 	private Map<Message, TimeStamp> mPendingResponse = new HashMap<Message, TimeStamp>();
 
 	private NativeService mService;
-	
+
+	private boolean mIsConnected;
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -57,10 +93,10 @@ public class MessageService extends Service {
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		//TODO check server configuration
-		
-		//TODO start back-end thread for send or listen server message
-		
+		// TODO check server configuration
+
+		// TODO start back-end thread for send or listen server message
+
 		// TODO Auto-generated method stub
 		return super.onStartCommand(intent, flags, startId);
 	}
@@ -70,27 +106,20 @@ public class MessageService extends Service {
 		super.onDestroy();
 		mService = null;
 		notificationListeners.clear();
-		//TODO destroy server connection
-		
-		
-		//TODO remove all PendingResponse
-		
-		//TODO send broadcast for service down
+		dispose();
+
+		// TODO remove all PendingResponse
+
 		Intent i = new Intent("com.carlocation.MessageServiceDown");
 		i.addCategory("com.carlocation");
 		this.sendBroadcast(i);
 		this.unregisterReceiver(mConnectionReceiver);
 	}
 
-
 	@Override
 	public IBinder onBind(Intent intent) {
 		return mService;
 	}
-	
-	
-	
-	
 
 	private BroadcastReceiver mConnectionReceiver = new BroadcastReceiver() {
 
@@ -98,35 +127,32 @@ public class MessageService extends Service {
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();
 
-            /**
-             * Temporary cancel to stop crash.
-             */
+			/**
+			 * Temporary cancel to stop crash.
+			 */
 
-			/*if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
-				ConnectivityManager connMgr = (ConnectivityManager) context
-						.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-				android.net.NetworkInfo wifi = connMgr
-						.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-
-				android.net.NetworkInfo mobile = connMgr
-						.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-				
-				//TODO if network changed need to re connect to server
-			}*/
+			/*
+			 * if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
+			 * ConnectivityManager connMgr = (ConnectivityManager) context
+			 * .getSystemService(Context.CONNECTIVITY_SERVICE);
+			 * 
+			 * android.net.NetworkInfo wifi = connMgr
+			 * .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			 * 
+			 * android.net.NetworkInfo mobile = connMgr
+			 * .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+			 * 
+			 * //TODO if network changed need to re connect to server }
+			 */
 		}
-		
+
 	};
-	
-	
-	
-	
-	
+
 	class TimeStamp {
 		Message message;
 		long timestamp;
 		ResponseListener listener;
-		
+
 		public TimeStamp(Message message, ResponseListener listener) {
 			super();
 			this.message = message;
@@ -134,20 +160,36 @@ public class MessageService extends Service {
 			this.timestamp = System.currentTimeMillis();
 		}
 
-		
 	}
-	
-	
+
 	class NativeService extends Binder implements IMessageService {
 
-		
-		
 		@Override
 		public void sendMessage(Message message) {
-			Log.e(TAG, "get message:"+ message);
-			// TODO Invoke send message.translate to format for network
-			// Send message
-			
+			Log.e(TAG, "get message:" + message);
+			if (message.getMessageType() == MessageType.AUTH_MESSAGE) {
+				AuthMessage am = (AuthMessage) message;
+				mSendChannel = getChannel(mServer, mPort, am.getUserName(),
+						am.getPassword());
+				TimeStamp ts = mPendingResponse.get(message);
+				if (mSendChannel != null && mSendChannel.isOpen()) {
+					mUserName = am.getUserName();
+					mPassword = am.getPassword();
+					if (ts != null) {
+						ts.listener.onResponse(new Notification(message,
+								Notification.NotificationType.RESPONSE,
+								Notification.Result.SUCCESS));
+					}
+				} else {
+					if (ts != null) {
+						ts.listener.onResponse(new Notification(message,
+								Notification.NotificationType.RESPONSE,
+								Notification.Result.FAILED));
+					}
+				}
+			} else {
+
+			}
 		}
 
 		@Override
@@ -156,11 +198,11 @@ public class MessageService extends Service {
 				throw new RuntimeException("message is null");
 			}
 			if (listener != null) {
-				mPendingResponse.put(message, new TimeStamp(message,listener));
+				mPendingResponse.put(message, new TimeStamp(message, listener));
 			} else {
-				Log.w(TAG, message +"  response listener is null");
+				Log.w(TAG, message + "  response listener is null");
 			}
-			
+
 			sendMessage(message);
 		}
 
@@ -178,10 +220,69 @@ public class MessageService extends Service {
 		public void unRegisterNotificationListener(NotificationListener listener) {
 			notificationListeners.remove(listener);
 		}
-		
+
 	}
-	
-	
-	
+
+	private Channel getChannel() {
+		return getChannel(mServer, mPort, mUserName, mPassword);
+	}
+
+	private Channel getChannel(String server, int port, String username,
+			String password) {
+		if (connectionFactory == null) {
+			connectionFactory = new ConnectionFactory();
+			connectionFactory.setHost(server);
+			connectionFactory.setUsername(username);
+			connectionFactory.setPassword(password);
+			connectionFactory.setPort(port);
+		}
+		try {
+			if (mConnection == null || !mConnection.isOpen()) {
+				mConnection = connectionFactory.newConnection();
+				mSendChannel = mConnection.createChannel();
+			}
+		} catch (IOException e) {
+			if (mConnection != null) {
+				try {
+					mConnection.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			Log.e(TAG, "Connection close failed", e);
+			return null;
+		}
+
+		if (mSendChannel == null || !mSendChannel.isOpen()) {
+			try {
+				mSendChannel = mConnection.createChannel();
+			} catch (IOException e) {
+				Log.e(TAG, "Create channel failed", e);
+			}
+		}
+		return mSendChannel;
+	}
+
+	private void dispose() {
+
+		// TODO notify consumer thread
+
+		if (mSendChannel != null && mSendChannel.isOpen()) {
+			try {
+				mSendChannel.close();
+			} catch (IOException e) {
+				Log.e(TAG, "Send Channel close failed", e);
+			}
+		}
+
+		if (mConnection != null && mConnection.isOpen()) {
+			try {
+				mConnection.close();
+			} catch (IOException e) {
+				Log.e(TAG, "Connection close failed", e);
+			}
+		}
+
+	}
 
 }
--- app/src/main/java/com/carlocation/comm/messaging/Notification.java
+++ app/src/main/java/com/carlocation/comm/messaging/Notification.java
@@ -4,11 +4,33 @@ public class Notification {
 	
 	public Message message;
 	
+	public NotificationType notiType;
+	
+	public Result result;
 	
 	public enum NotificationType {
 		REQUEST,
 		RESPONSE,
 		UNSOLICITED,
 	}
+	
+	
+	public enum Result {
+		SUCCESS,
+		FAILED,
+		TIME_OUT,
+		SERVER_RJECT;
+	}
+
+
+	public Notification(Message message, NotificationType notiType, Result result) {
+		super();
+		this.message = message;
+		this.notiType = notiType;
+		this.result = result;
+	}
+	
+	
+	
 
 }
